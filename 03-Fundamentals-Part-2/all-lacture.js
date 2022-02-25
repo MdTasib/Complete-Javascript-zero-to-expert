e3d5a61027d61615640c85926e14eb744b2d873c
MdTasib <ohidulalam442@gmail.com>
2022-02-25 15:33:35 +0600

    array intro

--- 03-Fundamentals-Part-2/all-lacture.js
+++ 03-Fundamentals-Part-2/all-lacture.js
@@ -84,3 +84,48 @@ function juiceMaker(apples, oranges) {
 
 console.log(juiceMaker(2, 3));
  */
+
+/* ///////////////////////////////////////
+// Introduction to Arrays
+const friend1 = "Rafi";
+const friend2 = "Rakib";
+const friend3 = "Walid";
+
+const friends = ["Rafi", "Rakib", "Walid"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "akib";
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const firstName = "Tasib";
+const jonas = [firstName, "Mohammad", 2037 - 1991, "web developer", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYeah) {
+	return 2037 - birthYeah;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+	calcAge(years[0]),
+	calcAge(years[1]),
+	calcAge(years[years.length - 1]),
+];
+console.log(ages);
+ */
