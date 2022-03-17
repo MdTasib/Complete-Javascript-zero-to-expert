c4226e6c3a6e6730cd4aa4cf1497fd6b4557b026
MdTasib <ohidulalam442@gmail.com>
2022-03-17 23:04:23 +0600

    7. Implementing the Game Logic

--- Browser-DOM-and-Events-Fundamentals/Number-Guess-Game/script.js
+++ Browser-DOM-and-Events-Fundamentals/Number-Guess-Game/script.js
@@ -1,9 +1,35 @@
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+// score
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
 	const guess = Number(document.querySelector(".guess").value);
-	console.log(guess);
-	console.log(!guess);
 
 	if (!guess) {
 		document.querySelector(".message").textContent = "⛔️ No number!";
+	} else if (secretNumber === guess) {
+		document.querySelector(".message").textContent = "🎉 Correct Number!";
+	}
+	// guesses too high
+	else if (secretNumber < guess) {
+		if (score > 1) {
+			document.querySelector(".message").textContent = "📈 To High!";
+			score--;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You Loss";
+		}
+	}
+	// guesses too low
+	else if (secretNumber > guess) {
+		if (score > 1) {
+			document.querySelector(".message").textContent = "📉 To Low!";
+			score--;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You Loss";
+		}
 	}
 });
