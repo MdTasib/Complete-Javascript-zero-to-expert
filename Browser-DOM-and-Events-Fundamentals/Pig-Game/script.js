608b8567d1fbdcded31c766cac9b187a2089ba96
MdTasib <ohidulalam442@gmail.com>
2022-04-03 17:00:19 +0600

    16. Rolling the Dice

--- Browser-DOM-and-Events-Fundamentals/Pig-Game/script.js
+++ Browser-DOM-and-Events-Fundamentals/Pig-Game/script.js
@@ -3,6 +3,8 @@
 // selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -12,6 +14,7 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", () => {
@@ -22,4 +25,13 @@ btnRoll.addEventListener("click", () => {
 	// display dice
 	diceEl.classList.remove("hidden");
 	diceEl.src = `dice-${dice}.png`;
+
+	// check for rolled 1
+	if (dice !== 1) {
+		// add dice to current score
+		currentScore += dice;
+		current0El.textContent = currentScore;
+	} else {
+		// switch next player
+	}
 });
