64c98c5b414d628908da6ae7da02f100b0405d12
MdTasib <ohidulalam442@gmail.com>
2022-04-08 17:37:06 +0600

    19. Resetting the Game

--- Browser-DOM-and-Events-Fundamentals/Pig-Game/script.js
+++ Browser-DOM-and-Events-Fundamentals/Pig-Game/script.js
@@ -13,14 +13,24 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+const init = function () {
+	const scores = [0, 0];
+	let currentScore = 0;
+	let activePlayer = 0;
+	let playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+	score0El.textContent = 0;
+	score1El.textContent = 0;
+	current0El.textContent = 0;
+	current1El.textContent = 0;
+
+	diceEl.classList.add("hidden");
+	player0El.classList.remove("player--winner");
+	player1El.classList.remove("player--winner");
+	player0El.classList.add("player--active");
+	player1El.classList.remove("player--active");
+};
+init();
 
 function switchPlayer() {
 	document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -78,3 +88,5 @@ btnHold.addEventListener("click", function () {
 		}
 	}
 });
+
+btnNew.addEventListener("click", function () {});
