965f398053ea1ff9ad71acaf617e798954156d72
MdTasib <ohidulalam442@gmail.com>
2022-08-07 16:33:46 +0600

    keys, values, entries

---
+++ 09. Data Structures, Modern Operators and Strings/object-keys-values-entries.js
@@ -0,0 +1,18 @@
+const openingHours = {
+	thu: {
+		open: 12,
+		close: 22,
+	},
+	fri: {
+		open: 11,
+		close: 23,
+	},
+	sat: {
+		open: 0, // Open 24 hours
+		close: 24,
+	},
+};
+
+console.log(Object.keys(openingHours));
+console.log(Object.values(openingHours));
+console.log(Object.entries(openingHours));
